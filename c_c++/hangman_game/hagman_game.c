66013f74f78764e73d05d9d27b89fa1175630006
ecocaval <ecocaval@hotmail.com>
2022-08-01 18:17:30 -0300

    Improving exercise code

--- c_c++/hangman_game/hagman_game.c
+++ c_c++/hangman_game/hagman_game.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define SECRET_WORD_SIZE 20
+#define SECRET_WORD_MAX_SIZE 20
+
+#define BLANK_SPACE ' '
+#define UNDERLINE_SPACE '_'
+
+#define SECRET_WORD "amarelo"
+
+unsigned short underline_counter;
 
 bool get_right;
 bool get_hanged;
 
-char secret_word[SECRET_WORD_SIZE];
-
 char user_guess;
+char secret_word[SECRET_WORD_MAX_SIZE];
+char word_displayed[SECRET_WORD_MAX_SIZE*2];
+char *ptr_word_displayed;
 
 void main()
 {
-    sprintf(secret_word, "melancia");
-
     print_game_header();
-    
+
+    sprintf(secret_word, SECRET_WORD);
+
+    underline_counter = strlen(secret_word);
+
+    for (int counter = 0; counter < strlen(secret_word); counter++)
+    {
+        word_displayed[counter * 2] = UNDERLINE_SPACE;
+        word_displayed[counter * 2 + 1] = BLANK_SPACE;
+    }   
+
     while(!get_right && !get_hanged)
     {   
+        printf("\n Please enter your guess: ");
+        scanf(" %c", &user_guess); // space between %c to avoid "enter key" on buffer;
 
+        for (int counter = 0; counter < strlen(secret_word); counter++)
+        {
+            if(secret_word[counter] == user_guess)
+            {
+                word_displayed[counter*2] = user_guess;
+                underline_counter--;
+            }
+        }
+
+        printf("%s\n", word_displayed);
+        print_mark();
+
+        if (underline_counter == 0) get_right = true;
     }
 }
 
