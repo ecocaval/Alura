f6b5e592acd43d353d9f94f534b381aa7eddfbe5
ecocaval <ecocaval@hotmail.com>
2022-08-01 19:48:15 -0300

    improving exercise code

--- c_c++/hangman_game/hagman_game.c
+++ c_c++/hangman_game/hagman_game.c
@@ -7,7 +7,7 @@
 #define BLANK_SPACE ' '
 #define UNDERLINE_SPACE '_'
 
-#define SECRET_WORD "amarelo"
+#define SECRET_WORD "fodasse"
 
 unsigned short underline_counter;
 
@@ -16,41 +16,24 @@ bool get_hanged;
 
 char user_guess;
 char secret_word[SECRET_WORD_MAX_SIZE];
-char word_displayed[SECRET_WORD_MAX_SIZE*2];
-char *ptr_word_displayed;
+char word_displayed[SECRET_WORD_MAX_SIZE * 2];
 
 void main()
 {
     print_game_header();
 
-    sprintf(secret_word, SECRET_WORD);
-
-    underline_counter = strlen(secret_word);
-
-    for (int counter = 0; counter < strlen(secret_word); counter++)
-    {
-        word_displayed[counter * 2] = UNDERLINE_SPACE;
-        word_displayed[counter * 2 + 1] = BLANK_SPACE;
-    }   
+    get_secret_word();
 
     while(!get_right && !get_hanged)
     {   
-        printf("\n Please enter your guess: ");
-        scanf(" %c", &user_guess); // space between %c to avoid "enter key" on buffer;
+        get_user_guess();
 
-        for (int counter = 0; counter < strlen(secret_word); counter++)
-        {
-            if(secret_word[counter] == user_guess)
-            {
-                word_displayed[counter*2] = user_guess;
-                underline_counter--;
-            }
-        }
+        compare_guess_to_secret_word();
 
         printf("%s\n", word_displayed);
         print_mark();
 
-        if (underline_counter == 0) get_right = true;
+        check_stop_condition();
     }
 }
 
@@ -64,4 +47,40 @@ void print_game_header()
     print_mark();
     printf("*Welcome to our hangman game!");
     print_mark();
+}
+
+void get_secret_word()
+{
+    sprintf(secret_word, SECRET_WORD);
+    
+    underline_counter = strlen(secret_word);
+
+    for(int counter = 0; counter < strlen(secret_word); counter++)
+    {
+        word_displayed[counter * 2] = UNDERLINE_SPACE;
+        word_displayed[counter * 2 + 1] = BLANK_SPACE;
+    }   
+}
+
+void get_user_guess()
+{
+    printf("\n Please enter your guess: ");
+    scanf(" %c", &user_guess); // space between %c to avoid "enter key" on buffer;
+}
+
+void compare_guess_to_secret_word()
+{
+    for(int counter = 0; counter < strlen(secret_word); counter++)
+    {
+        if(secret_word[counter] == user_guess)
+        {
+            word_displayed[counter*2] = user_guess;
+            underline_counter--;
+        }
+    }
+}
+
+void check_stop_condition()
+{
+    if(underline_counter == 0) get_right = true;
 }
