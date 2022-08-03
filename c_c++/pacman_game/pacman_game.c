8dbdbca135e1a2af6e9f3da8b16d294ba56369a5
ecocaval <ecocaval@hotmail.com>
2022-08-03 19:45:14 -0300

    working scan_game_map()

--- c_c++/pacman_game/pacman_game.c
+++ c_c++/pacman_game/pacman_game.c
@@ -5,13 +5,12 @@
 #include <stdio.h>
 #include "pacman_game.h"
 
-int game_total_columns;
-
 char map_analyser[ALPHABET_SIZE];
 char past_map_analyser[ALPHABET_SIZE];
 
 unsigned int rows_analysed = 1;
 unsigned int game_total_rows = 0;
+unsigned int game_total_columns = 0;
 
 void main()
 {
@@ -32,49 +31,54 @@ void scan_game_map()
     else
     {
         fseek(f_game_map, 0, SEEK_SET);
-
         fscanf(f_game_map, "%s\n", map_analyser);
 
-        printf("%s ", map_analyser);
+        game_total_columns = strlen(map_analyser);
+
+        printf("\n%s ", map_analyser);
         printf("Analizing row: %d\n", rows_analysed);
 
-        while (map_analyser[0] == SIDE_WALL)
-        {
-            rows_analysed++;
-
-            for(int i = 0; i < strlen(map_analyser); i++)
-            {
-                past_map_analyser[i] = map_analyser[i];
-            }
-
-            fscanf(f_game_map, "%s\n", map_analyser);
-
-            if(check_if_row_repeat()) 
-            {
-                rows_analysed--;
-                break;
-            }
-
-            printf("%s ", map_analyser);
-            printf("Analizing row: %d\n", rows_analysed);
-        }
+        count_map_rows(f_game_map);
 
         game_total_rows = rows_analysed;
 
         printf("\nMap total number of rows: %d", game_total_rows);
-        printf("\nMap total number of columns: %d\n", strlen(map_analyser));
-        
+        printf("\nMap total number of columns: %d\n", game_total_columns);
     }
     fclose(f_game_map);
+}
+
+void count_map_rows(FILE* map_ptr)
+{
+    while (map_analyser[0] == SIDE_WALL)
+    {
+        rows_analysed++;
+
+        for(int columns_counter = 0; columns_counter < game_total_columns; columns_counter++)
+        {
+            past_map_analyser[columns_counter] = map_analyser[columns_counter];
+        }
+
+        fscanf(map_ptr, "%s\n", map_analyser);
+
+        if(check_if_row_repeat()) 
+        {
+            rows_analysed--;
+            break;
+        }
+
+        printf("%s ", map_analyser);
+        printf("Analizing row: %d\n", rows_analysed);
+    }
 }
 
 int check_if_row_repeat()
 {
     int right_words_counter = 0;
 
-    for(int i = 0; i < strlen(map_analyser); i++)
+    for(int columns_counter = 0; columns_counter < strlen(map_analyser); columns_counter++)
     {
-        if(past_map_analyser[i] == map_analyser[i]) right_words_counter++;   
+        if(past_map_analyser[columns_counter] == map_analyser[columns_counter]) right_words_counter++;   
     }
 
     if(strlen(map_analyser) == right_words_counter)
--- c_c++/pacman_game/pacman_game.h
+++ c_c++/pacman_game/pacman_game.h
@@ -2,11 +2,15 @@
  * @brief pacman_game -- created in 03/08/2022 (Brazil) by Érico
 */
 
+#include <stdio.h>
+
 #define ALPHABET_SIZE 26
 #define SIDE_WALL '|'
 
 #define BLANK_SPACE ' '
 
 void scan_game_map();
+
+void count_map_rows(FILE* file_ptr);
 
 int check_if_row_repeat();
