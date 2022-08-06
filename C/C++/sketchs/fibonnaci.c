78abb0a6de3254de0d1942a04cdd4d003afdc049
ecocaval <ecocaval@hotmail.com>
2022-08-06 18:00:22 -0300

    improving code

--- C/C++/sketchs/fibonnaci.c
+++ C/C++/sketchs/fibonnaci.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define TOP_LIMIT 1000
+
 void main()
 {
     int n0 = 0;
@@ -16,7 +18,7 @@ void call_fibonacci(int n0, int n1)
     int n2;
     n2 = n1 + n0;
 
-    if(n2 > 1000) return;
+    if(n2 > TOP_LIMIT) return;
 
     printf("\n%d\n", n2);
     
