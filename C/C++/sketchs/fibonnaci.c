ae4938af3a72dbcc22684025206c4be36cde926f
ecocaval <ecocaval@hotmail.com>
2022-08-06 17:58:44 -0300

    improving code and adding some sketchs

---
+++ C/C++/sketchs/fibonnaci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+    int n0 = 0;
+    int n1 = 1;
+
+    printf("\n%d\n", n0);
+    printf("\n%d\n", n1);
+
+    call_fibonacci(0, 1);
+}
+
+void call_fibonacci(int n0, int n1)
+{
+    int n2;
+    n2 = n1 + n0;
+
+    if(n2 > 1000) return;
+
+    printf("\n%d\n", n2);
+    
+    call_fibonacci(n1, n2);
+}
