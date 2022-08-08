e01fe0e0c7ffc2b086c56f257a7338aafd96fa0b
erico <ecocaval@hotmail.com>
2022-08-08 20:19:25 -0300

    starting to work on JavaScript code

---
+++ JavaScript/fundamentos/exercise_1.js
@@ -0,0 +1,38 @@
+// Javascript types
+
+const myNumber = 1;
+const myNumber2 = 2;
+
+const mathOperation = myNumber + myNumber2;
+const mathOperation2 = myNumber * myNumber2;
+const mathOperation3 = myNumber - myNumber2;
+const mathOperation4 = myNumber / myNumber2;
+
+console.log(mathOperation);
+console.log(mathOperation2);
+console.log(mathOperation3);
+console.log(mathOperation4);
+
+const decNumber = .5;
+const decNumber2 = 1.5;
+
+console.log(decNumber*decNumber2);
+
+const letter = 'a';
+
+console.log(letter);
+console.log(letter*decNumber); // Must display NaN => Not a Number
+
+const phrase = "test"
+
+console.log(phrase);
+
+const booleanTest = true;
+
+console.log(booleanTest);
+
+console.log(Math.PI); // pi number
+
+const testNumber = 5e-2;
+
+console.log(testNumber);
---
+++ JavaScript/fundamentos/exercise_2.js
@@ -0,0 +1,7 @@
+function Multiply(Number1, Number2) 
+{
+    const Number3 = Number1 * Number2;
+    return Number3;
+}
+
+console.log(Multiply(5,2));
