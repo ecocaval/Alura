3ffbfd0d932bf6a67e6e355008af88f829daf493
erico <ecocaval@hotmail.com>
2022-08-08 23:23:01 -0300

    working on new javaScript exercises

--- JavaScript/fundamentos/exercise_3.js
+++ JavaScript/fundamentos/exercise_3.js
@@ -25,8 +25,8 @@
 
 /*********************************************/
 
-let numberNumber = 313;
-let numberString = '313';
+// let numberNumber = 313;
+// let numberString = '313';
 
 // implicit conversion 
 
@@ -35,6 +35,74 @@ let numberString = '313';
 
 // explicit conversion
 
-console.log(String(numberNumber) + numberString);
+// // model 1
+// console.log(String(numberNumber) + numberString);
+// console.log(numberNumber + Number(numberString));
 
-console.log(numberNumber + Number(numberString));
+// // model 2
+// console.log(numberNumber.toString() + numberString);
+// console.log(numberNumber + (+ numberString)); // not usual to use this type of conversion
+
+/*********************************************/
+
+// let numberTest = 2;
+
+// if (numberTest === 1) {
+//     console.log('ok!');
+// } else {
+//     console.error('error in code!');
+// }
+
+/*********************************************/
+
+// let numberTest = 2;
+
+// console.time();
+
+// if (numberTest === 1) {
+//     console.log('ok!');
+// } else {
+//     console.error(new Error('error in code!'));
+// }
+
+// console.timeEnd();
+
+/*********************************************/
+
+// let numberTested1 = 1;
+// let numberTested2 = 2;
+
+// const comparingNumbers = numberTested1 > numberTested2;
+
+// console.assert(comparingNumbers, 'number1 is not greater than number2!');
+
+// console.clear();
+
+/*********************************************/
+
+// console.count(1);
+// console.count(1);
+// console.count(1);
+// console.count(1);
+// console.count('123');
+// console.count('123');
+// console.countReset(1);
+// console.countReset('123');
+// console.count(1);
+// console.count(1);
+// console.count('123');
+// console.count('123');
+
+/*********************************************/
+
+// console.table([{a: 1, b: 'Y'} , {a: 'Z', b: 0}]);
+
+// console.table([{Nome1: 'Regina', Nome2: 'Silvia'} , {Nome1: 100.45, Nome2: 80.59}]);
+
+/*********************************************/
+
+let number = 313;
+let numberString = '313';
+
+console.log(number != numberString);
+console.log(number !== numberString);
---
+++ JavaScript/fundamentos/exercise_4.js
@@ -0,0 +1,22 @@
+// let listaDeFuncionarios = {};
+
+// listaDeFuncionarios.nome1 = 'Roberto';
+// listaDeFuncionarios.nome2 = 'Valdea';
+
+// console.log(listaDeFuncionarios);
+// console.log(listaDeFuncionarios.nome1);
+// console.log(listaDeFuncionarios.nome2);
+
+/***************************************/
+
+let nomes = {};
+let listaDeFuncionarios = {nomes};
+
+listaDeFuncionarios.nomes.nome1 = 'Andrea';
+listaDeFuncionarios.nomes.nome2 = 'João';
+
+console.log(listaDeFuncionarios);
+console.log(listaDeFuncionarios.nomes);
+console.log(listaDeFuncionarios.nomes.nome1);
+console.log(listaDeFuncionarios.nomes.nome2);
+
