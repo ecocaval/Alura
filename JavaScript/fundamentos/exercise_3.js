3f76fabc27f8cb7592cefd1f7ce60f17ac52ac93
erico <ecocaval@hotmail.com>
2022-08-08 22:06:20 -0300

    working on new exercises

--- JavaScript/fundamentos/exercise_3.js
+++ JavaScript/fundamentos/exercise_3.js
@@ -1,16 +1,40 @@
-let polygon = 'triangle';
-let polygonHeight = 3;
-let polygonLenght = 10;
+// let polygon = 'triangle';
+// let polygonHeight = 3;
+// let polygonLenght = 10;
 
-var polygonArea;
+// var polygonArea;
 
-if(polygon === 'triangle')
-{
-    polygonArea = polygonHeight * polygonLenght / 2;
-} 
-else
-{
-    polygonArea = polygonHeight * polygonLenght;
-}
+// if(polygon === 'triangle') {
+//     polygonArea = polygonHeight * polygonLenght / 2;
+// } 
+// else {
+//     polygonArea = polygonHeight * polygonLenght;
+// }
 
-console.log(polygonArea);
+// console.log(polygonArea);
+
+/*********************************************/
+
+// let textExample = 'test';
+// let numberExample = 5;
+// let undefinedExample;
+
+// console.log(typeof textExample);
+// console.log(typeof numberExample);
+// console.log(typeof undefinedExample);
+
+/*********************************************/
+
+let numberNumber = 313;
+let numberString = '313';
+
+// implicit conversion 
+
+// console.log(numberNumber == numberString); 
+// console.log(numberNumber === numberString);
+
+// explicit conversion
+
+console.log(String(numberNumber) + numberString);
+
+console.log(numberNumber + Number(numberString));
