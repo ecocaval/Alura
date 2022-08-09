19f323b2f51c5e217945e3d5ca62951255882aa8
erico <ecocaval@hotmail.com>
2022-08-09 02:35:23 -0300

    improving exercises code

--- JavaScript/Arrays/highOrderFunctions.js
+++ JavaScript/Arrays/highOrderFunctions.js
@@ -1,23 +1,59 @@
 /*******************************************/
 
-const usuarioLogin = (nomeUsuario) => {
-    let array = [];
-    for(let i =0; i < 90000; i++) {
-        array.push(i);
-    }
-    return `${nomeUsuario} logou com sucesso!`;
-}
+// const usuarioLogin = (nomeUsuario, cargo) => {
+//     if(autenticaCargo(cargo)) {
+//         return acesso(nomeUsuario);
+//     } else {
+//         return 0;
+//     }
+// }
+
+// const acesso = (nomeUsuario) => {
+//     return `${nomeUsuario} logou com sucesso!`;
+// }
+
+// const autenticaCargo = (cargo) => {
+//     let array = [];
+//     for(let i = 0; i < cargo; i++) {
+//         array.push(i);
+//     }
+//     return true;
+// }
+
+// console.log(usuarioLogin('lucas'));
 
 /*******************************************/
 
-console.log(usuarioLogin('lucas'));
+// function userLogin(username) {
+//     let array = []
+//     for(let i = 0; i < 90000; i++) {
+//         array.push[i];
+//     }
+//     return `${username} has logged sucessfully!`;
+// }
 
-function userLogin(username) {
+// console.log(userLogin('lucas'));
+
+/*******************************************/
+
+const acesso = (nome) => {
+    return `${nome} logou com sucesso no sistema!`
+  }
+  
+  const autentica = (cargo) => {
     let array = []
-    for(let i = 0; i < 90000; i++) {
-        array.push[i];
+    for( i = 0; i < cargo; i++){
+      array.push(i)
     }
-    return `${username} has logged sucessfully!`;
-}
-
-console.log(userLogin('lucas'));
+    return true;
+  }
+  
+  const login = (pessoa, autentica) => {
+    if(pessoa.cargo === `funcionario`) {
+       autentica(90000)
+    } else if(pessoa.cargo === `diretoria`) {
+       autentica(900000)
+    }
+   return acesso(pessoa.nome)
+  }
+  
