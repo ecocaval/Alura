0bae994b3e4bb5c259f88c870fe60935b4f374e4
erico <ecocaval@hotmail.com>
2022-08-08 21:04:28 -0300

    working on new wxercises

--- JavaScript/fundamentos/exercise_1.js
+++ JavaScript/fundamentos/exercise_1.js
@@ -1,4 +1,4 @@
-// Javascript types
+//-----------------------------------------//
 
 const myNumber = 1;
 const myNumber2 = 2;
@@ -13,10 +13,14 @@ console.log(mathOperation2);
 console.log(mathOperation3);
 console.log(mathOperation4);
 
+//-----------------------------------------//
+
 const decNumber = .5;
 const decNumber2 = 1.5;
 
 console.log(decNumber*decNumber2);
+
+//-----------------------------------------//
 
 const letter = 'a';
 
@@ -27,11 +31,15 @@ const phrase = "test"
 
 console.log(phrase);
 
+//-----------------------------------------//
+
 const booleanTest = true;
 
 console.log(booleanTest);
 
 console.log(Math.PI); // pi number
+
+//-----------------------------------------//
 
 const testNumber = 5e-2;
 
--- JavaScript/fundamentos/exercise_2.js
+++ JavaScript/fundamentos/exercise_2.js
@@ -5,3 +5,25 @@ function Multiply(Number1, Number2)
 }
 
 console.log(Multiply(5,2));
+
+//-----------------------------------------// 
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+//-----------------------------------------//
+
+var x = 'Mozilla';
+var empty = '';
+
+console.log('Mozilla possui ' + x.length + ' unidades de código de comprimento');
+/* "Mozilla possui 7 unidades de código de comprimento" */
+
+console.log('A string vazia possui um comprimento de ' + empty.length);
+/* "A string vazia possui um comprimento de 0" */
+
+//-----------------------------------------//
