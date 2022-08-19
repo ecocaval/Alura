3bdd5b56b6259d07167ad5edc03e5697ecbdb90c
ecocaval <ecocaval@hotmail.com>
2022-08-19 14:44:17 -0300

    improving working code

--- JavaScript/Arrays/atualizando-lista.js
+++ JavaScript/Arrays/atualizando-lista.js
@@ -1,22 +1,31 @@
 const listaDeAlunos = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo']
 const copiaListaOriginal = []
 
-console.log(listaDeAlunos)
-
 const copiaLista = (listaOriginal, copiaDaLista) => {
     for(let i = 0; i < listaOriginal.length; i++) {
         copiaDaLista.push(listaOriginal[i])
     }
 }
 
+const limpaListaCopia = (copiaDaLista) => {
+    const tamanhoDaCopia = copiaDaLista.length
+    for(let i = 0; i < tamanhoDaCopia; i++) {
+        copiaDaLista.pop()
+    }
+}
+
 copiaLista(listaDeAlunos, copiaListaOriginal)
 
+console.log(copiaListaOriginal)
+
 for(let i = 0; i < copiaListaOriginal.length; i++) {
-    if(copiaListaOriginal[i] == 'Ana') {
+    if(copiaListaOriginal[i] == 'Ana' || copiaListaOriginal[i] == 'Caio') {
         for(let j = i; j < copiaListaOriginal.length; j++){
             listaDeAlunos.pop()
         }
     }
+    limpaListaCopia(copiaListaOriginal)
+    copiaLista(listaDeAlunos, copiaListaOriginal)
 }
 
 console.log(listaDeAlunos)
