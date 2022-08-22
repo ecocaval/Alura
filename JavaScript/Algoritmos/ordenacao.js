0fcf27bd33e9451e197c4afc36bdcb33272556aa
ecocaval <ecocaval@hotmail.com>
2022-08-22 16:33:04 -0300

    working on ordenation algorithm with objects

--- JavaScript/Algoritmos/ordenacao.js
+++ JavaScript/Algoritmos/ordenacao.js
@@ -1,5 +1,26 @@
-const booksPrices = [25, 15, 30, 50, 45, 20, 100, 48, 90, 44.2, 31.7, 55.8, 100.5]
+class Book {
+    constructor(name, author, price) {
+        this.name = name
+        this.author = author
+        this.price = price
+    }
+}
 
+const booksList = []
+
+const homoDeus = new Book('Homo Deus','Harari', 50)
+booksList.push(homoDeus)
+
+const homoSapiens = new Book('Homo Sapiens', 'Harari', 75)
+booksList.push(homoSapiens)
+
+const antiCristo = new Book('Anti Cristo', 'Nietzsche', 60)
+booksList.push(antiCristo)
+
+// think how to push prices from book list
+
+const booksPrices = [25, 15, 30, 50, 45, 20, 100, 48, 90, 44.2, 31.7, 55.8, 100.5]
+ 
 console.log(booksPrices)
 
 booksPrices.forEach(bookprice => {
@@ -17,4 +38,3 @@ booksPrices.forEach(bookprice => {
 })
 
 console.log(booksPrices)
-
