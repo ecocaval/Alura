2930622ceda216e0489602ac011b6f4925567543
ecocaval <ecocaval@hotmail.com>
2022-08-24 14:34:51 -0300

    working on new challenge prototype

--- JavaScript/Algoritmos/Algoritmos - 2/arquivos-base/aula-1/index.js
+++ JavaScript/Algoritmos/Algoritmos - 2/arquivos-base/aula-1/index.js
@@ -1,3 +1,29 @@
 const livrosAnalisados = require('./arrays.js')
+// console.log(livrosAnalisados)
+// console.log((livrosAnalisados.edFolha[atualListaFolha]).preco)
 
-console.log(livrosAnalisados)
+let livrosOrdenados = []
+
+let atualListaFolha = 0 
+let atualListaGalho = 0
+ 
+const listaFolhaTamanho = livrosAnalisados.edFolha.length
+const listaGalhoTamanho = livrosAnalisados.edGalho.length
+ 
+const atualFinalMax = listaFolhaTamanho + listaGalhoTamanho
+// console.log(atualFinalMax)
+
+for(let atualFinal = 0; atualFinal < 4; atualFinal++) {
+    if((livrosAnalisados.edFolha[atualListaFolha]).preco < livrosAnalisados.edGalho[atualListaGalho].preco) {
+        livrosOrdenados.push(livrosAnalisados.edFolha[atualListaFolha])
+        atualListaFolha++
+        continue
+    }
+    
+    livrosOrdenados.push(livrosAnalisados.edGalho[atualListaGalho])
+    atualListaGalho++
+}
+
+console.log(livrosOrdenados)
+
+// console.log(livrosOrdenados)
