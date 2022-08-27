e2b3dc11d3ec0987ae20cf2c110b27c103554412
ecocaval <ecocaval@hotmail.com>
2022-08-27 18:39:30 -0300

    cleaning code

--- javaScript/cursoAlgoritmos/Algoritmos - 2/aula-4-QuickSearch/index.js
+++ javaScript/cursoAlgoritmos/Algoritmos - 2/aula-4-QuickSearch/index.js
@@ -1,29 +1,22 @@
 const booksOrdered = require('./arrayOrdenado')
-console.log('\nOriginal Book List:\n')
-console.log(booksOrdered)
-console.log('\n*******************\n')
 
 function searchBookBasedOnPrice (booksOrderedArr, priceSearched) {  
-
-    console.log(booksOrderedArr)
 
     const arrMiddleIndex = Math.floor(booksOrderedArr.length / 2)
     const priceFromMiddleBook = booksOrderedArr[arrMiddleIndex].price
 
-    if(arrMiddleIndex === 0) return booksOrderedArr // guard function for recursivity
+    if(arrMiddleIndex === 0) return booksOrderedArr; // guard function for recursivity
+
+    if(priceFromMiddleBook === priceSearched) return booksOrderedArr[arrMiddleIndex]; 
 
     if(priceFromMiddleBook > priceSearched) {
-
         booksOrderedArr = searchBookBasedOnPrice(booksOrderedArr.slice(0, arrMiddleIndex), priceSearched)
-
-    } else if((priceFromMiddleBook < priceSearched)) {
-
+    } else {
         booksOrderedArr = searchBookBasedOnPrice(booksOrderedArr.slice(arrMiddleIndex), priceSearched)
-
-    } else return booksOrderedArr[arrMiddleIndex];
+    }
 
     return booksOrderedArr
 }   
 
-console.log(searchBookBasedOnPrice(booksOrdered, 30))
+console.log(searchBookBasedOnPrice(booksOrdered, 33))
 
