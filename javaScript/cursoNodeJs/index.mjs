81d5ef9afd6e7ba1fb2dc5fd5c0067a5f6a7e5d2
erico <ecocaval@hotmail.com>
2022-08-28 16:49:27 -0300

    working function with promise

--- javaScript/cursoNodeJs/index.mjs
+++ javaScript/cursoNodeJs/index.mjs
@@ -1,5 +1,3 @@
-const chalk = require("chalk"); // must be installed through npm install chalk
-const fs = require('fs'); // there's no need to install fs
 
 /*
     fs refers to 'file system'  -> fs.readFile(file, [encoding], [callback]);
@@ -13,19 +11,36 @@ const fs = require('fs'); // there's no need to install fs
         }
 */
 
+const chalk = require("chalk"); // must be installed through npm install chalk
+const fs = require('fs'); // there's no need to install fs
+
 function treatError (err) {
-    throw new Error(chalk.red(err.code, "There's no reading file in this directory!"))
+    throw new Error(chalk.red(err.code, "There's no reading file in this directory!"));
 }
 
 function catchFile (fileToCatchPath) {
     const encoding = 'utf-8';
-    const callBackCatchFIle = (err, textReceived) => {
-        if(err) {
-            return treatError(err)
-        }
-        return console.log(chalk.yellow(textReceived));
-    };
-    fs.readFile(fileToCatchPath, encoding, callBackCatchFIle);
+
+    fs.promises.readFile(fileToCatchPath, encoding)
+    
+    .then((textFomFile) => {
+        console.log(chalk.yellow(textFomFile));
+    })
+
+    .catch((err) => {
+        treatError(err);
+    })
 }
 
-catchFile('./arquivos');
+catchFile('./arquivos/texto1.md');
+
+// function catchFile (fileToCatchPath) {
+//     const encoding = 'utf-8';
+//     const callBackCatchFIle = (err, textReceived) => {
+//         if(err) {
+//             return treatError(err)
+//         }
+//         return console.log(chalk.yellow(textReceived));
+//     };
+//     fs.readFile(fileToCatchPath, encoding, callBackCatchFIle);
+// }
