71f45d081771db05a9f32b794352fb1b574b0c9f
erico <ecocaval@hotmail.com>
2022-08-28 15:39:40 -0300

    organizing code using chalk and fs

--- javaScript/cursoNodeJs/index.mjs
+++ javaScript/cursoNodeJs/index.mjs
@@ -1,3 +1,19 @@
-const chalk = require("chalk")
+const chalk = require("chalk"); // must be installed through npm install chalk
+const fs = require('fs'); // there's no need to install fs
 
-console.log(chalk.blue(' Hello world!\n'), chalk.green('This is a test using chalk!\n'), chalk.red('Lula #13'));
+/*
+    fs refers to 'file system'  -> fs.readFile(file, [encoding], [callback]);
+    obs: file = (string) filepath of the file to read;
+        encoding = will normally refer to 'UTF-8';
+        callback = function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(data);
+        }
+*/
+
+function catchFile (fileToCatchPath) {
+    const encoding = 'utf-8';
+    fs.readFile(fileToCatchPath, encoding, );
+}
