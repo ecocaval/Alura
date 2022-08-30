8fa9f691fc4837054c9c270701ae70d4dfdb5e6d
erico <ecocaval@hotmail.com>
2022-08-30 20:14:59 -0300

    adding more comments about regular expressions

--- javaScript/cursoNodeJs/parte 2/index.mjs
+++ javaScript/cursoNodeJs/parte 2/index.mjs
@@ -23,8 +23,23 @@ readFile('./arquivos/texto1.md');
 
 /*
     EXPRESSOES REGULARES:
-    \[[\w]*\} ----> seleciona todas (*) as palavras (\w) dentro de (\[) e (\]) -> note que o '\' 
-    é para que a [ e a ] nao sejam consfundidas com um vetor).
-*/
+    1) \[[\w]*\} ----> seleciona todas (*) as palavras (\w) dentro de (\[) e (\]) -> note que o '\' 
+    é para que a [ e a ] nao sejam consfundidas com um vetor). Para o nosso caso o problema desta
+    abordagem é que não seram considerados caracteres especiais incluidos dentro de '[]' no caso de
+    '<input>' por exemplo, para resolver isso podemos realizar a expressão da seguinte maneira:
+
+    2) \[[^\]*\] ----> selecionas tudo (*) que começa em [ e não termina com ] (^\]) e vai até ]. 
+
+    agora querendo encontrar os links kttps entre ()
+
+    3) \(https?:\/\/[^$#\s].[^\s]*\)  ---->  seleciona tudo que começa com '(https?://' + tudo que nao
+    os '[]' aqui       \s faz referencia |   for '$', '#', ' ' (representado por \s) + '.' + tudo que
+    criam uma classe.  a "white space". |    nao for ' ' até ')' (o qual também precisa iniciar com \).
+
+    Para declarar una expressão regular em javascript utilizamos '//'
+
+    ex: const regex = /[a-zA-z\s]/;
+        let regex = new RegExp("[a-zA-z\s]");
+*/                
 
 
