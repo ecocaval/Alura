da557476c2e2f4e55b4bd87b383be15eb1708411
erico <ecocaval@hotmail.com>
2022-08-31 00:12:18 -0300

    adding new code

--- javaScript/cursoNodeJs/parte 2/index.mjs
+++ javaScript/cursoNodeJs/parte 2/index.mjs
@@ -1,6 +1,13 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+class Link {
+    constructor(linkName, url) {
+        this.linkName = linkName;
+        this.url = url;
+    }
+}
+
 function treatError (err) {
     return console.log(chalk.red(`An error was detected!\n${err}`));
 }
@@ -18,18 +25,20 @@ async function readFile (filePath) {
         console.log('Finished reading file...')
     }
 }
+// readFile('./arquivos/texto1.md');
 
 function linksExtract (textToExtractLinks) {
-    const regEx = /\[([^]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm; //gm refers to global and multi-line
+    const regEx = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm; //gm refers to global and multi-line
     const extractedLinks = textToExtractLinks.match(regEx);
-    console.log(extractedLinks);
+    // console.log((extractedLinks));
+    
+    return extractedLinks;
 }
-
-// readFile('./arquivos/texto1.md');
 
 const texto1 = 'A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo. São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
 
-linksExtract(texto1);
+const linksExtractedArr = linksExtract(texto1);
+console.log(linksExtractedArr);
 
 /*
     EXPRESSOES REGULARES:
@@ -38,11 +47,11 @@ linksExtract(texto1);
     abordagem é que não seram considerados caracteres especiais incluidos dentro de '[]' no caso de
     '<input>' por exemplo, para resolver isso podemos realizar a expressão da seguinte maneira:
 
-    2) \[[^\]*\] ----> selecionas tudo (*) que começa em [ e não termina com ] (^\]) e vai até ]. 
+    2) \[([^\]]*)\]----> selecionas tudo (*) que começa em [ e não termina com ] (^\]) e vai até ]. 
 
     agora querendo encontrar os links kttps entre ()
 
-    3) \(https?:\/\/[^$#\s].[^\s]*\)  ---->  seleciona tudo que começa com '(https?://' + tudo que nao
+    3) \((https?:\/\/[^$#\s].[^\s]*)\)/gm ---->  seleciona tudo que começa com '(https?://' + tudo que nao
     os '[]' aqui       \s faz referencia |   for '$', '#', ' ' (representado por \s) + '.' + tudo que
     criam uma classe.  a "white space". |    nao for ' ' até ')' (o qual também precisa iniciar com \).
 
