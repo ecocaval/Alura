a13d6e10073d9de46cce95ae48cc414f82caa9e9
erico <ecocaval@hotmail.com>
2022-09-01 12:59:47 -0300

    improving url extractor function

--- javaScript/cursoNodeJs/parte 2/httpValidation.js
+++ javaScript/cursoNodeJs/parte 2/httpValidation.js
@@ -1,5 +1,20 @@
 async function validaURLs (arrayDeLinksParaValidar) {
-    console.log(arrayDeLinksParaValidar);
+
+    const arrURLs = []
+
+    const geraArrayDeURLs = (arr) => {
+        arr.forEach(element => {
+            arrURLs.push((Object.values(element))[0]);
+        });
+        // console.log(arrURLs);
+    }
+
+    geraArrayDeURLs(arrayDeLinksParaValidar);
+
+    arrURLs.forEach(URL => {
+        console.log(URL);
+    })
+    
 }
 
 module.exports = validaURLs
