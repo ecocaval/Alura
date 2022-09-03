2bc4680a5d81c0bde531e2b8e6a4afbcf13711d8
erico <ecocaval@hotmail.com>
2022-09-03 01:27:42 -0300

    starting new code

---
+++ javaScript/cursoOOP/user.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(name, email, birthday, role, activeInSystem) {
+        this.name = name;
+        this.email = email;
+        this.birthday = birthday;
+        this.role = role;
+        this.active = activeInSystem;
+    }
+}
